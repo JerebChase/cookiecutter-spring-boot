46700e88be0605e02366d1cc5ff016c2152d2656
Jeremy Brown <chase208@4JLT.com>
2024-12-11 16:11:23 -0500

    Updating to replace values with cookiecutter variables

---
+++ {{cookiecutter-name}}/src/test/java/{{cookiecutter.group}}/{{cookiecutter.name}}/HomeControllerTests.java
@@ -0,0 +1,14 @@
+package {{cookiecutter.group}}.{{cookiecutter.name}}.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeControllerTests {
+
+	@Test
+	public void home__onSuccess_returnsHelloWorld() throws Exception {
+        HomeController homeController = new HomeController();
+        assertEquals("Hello world", homeController.home());
+    }
+}
