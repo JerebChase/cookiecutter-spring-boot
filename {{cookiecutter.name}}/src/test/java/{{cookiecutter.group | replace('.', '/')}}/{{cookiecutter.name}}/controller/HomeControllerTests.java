4ddd55fbc20eba5523d4ac18652ff2dc765daa8e
Jeremy Brown <chase208@4JLT.com>
2024-12-12 15:10:19 -0500

    Updates to get the spring boot project to build

--- {{cookiecutter.name}}/src/test/java/{{cookiecutter.group | replace('.', '/')}}/{{cookiecutter.name}}/controller/HomeControllerTests.java
+++ {{cookiecutter.name}}/src/test/java/{{cookiecutter.group | replace('.', '/')}}/{{cookiecutter.name}}/controller/HomeControllerTests.java
@@ -1,5 +1,6 @@
 package {{cookiecutter.group}}.{{cookiecutter.name}}.controller;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
